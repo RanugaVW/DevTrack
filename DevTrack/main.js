d95dce876360fa72b7dd56bd660c1a273abe2e5f
Ranuga Weerasekara <ranugaweerasekara2@gmail.com>
2026-07-12 10:45:34 +0530

    feat: add Taskmanager data model with localStorage

--- DevTrack/main.js
+++ DevTrack/main.js
@@ -1,3 +1,44 @@
+const TaskManager = {
+	tasks: JSON.parse(localStorage.getItem('devtrack-tasks') || '[]'),
+
+	add(text) {
+		const task = {
+			id: Date.now(),
+			text,
+			done: false,
+			createdAt: new Date().toISOString(),
+		};
+
+		this.tasks.push(task);
+		this.save();
+		return task;
+	},
+
+	toggle(id) {
+		const task = this.tasks.find((item) => item.id === id);
+
+		if (task) {
+			task.done = !task.done;
+			this.save();
+		}
+
+		return task;
+	},
+
+	delete(id) {
+		this.tasks = this.tasks.filter((task) => task.id !== id);
+		this.save();
+	},
+
+	save() {
+		localStorage.setItem('devtrack-tasks', JSON.stringify(this.tasks));
+	},
+
+	getAll() {
+		return this.tasks;
+	},
+};
+
 const todayDateElement = document.getElementById('today-date');
 
 if (todayDateElement) {
